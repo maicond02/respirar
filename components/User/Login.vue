d87e97b1644ffdc54b47ed9fb73a995854cdde70
Maicon Alves <eumaicondouglas.md45@gmail.com>
2024-04-08 19:19:35 -0300

    Style: Clean code and update desing

--- components/User/Login.vue
+++ components/User/Login.vue
@@ -1,42 +1,27 @@
-<template #content>
-    <link rel="icon" href="/public/LOGORESP.png">
-    <div class="grid">
-        <card class="cardFundo col-12">
-            <template #content>
-                <div class="flex align-items-center justify-content-center">
-                    <card class="cardLogin col-8">
-                    <template #content>
-                        <div class="flex justify-content-center">
-                            <img class="imagemLogo" src="/public/LOGORESP.png"/>
-                            <card class="cardForm col-4 ">
-                                <template #content>
-                                    <h1 class="m-4 flex justify-content-center align-content-start">Login</h1>
-                                    <label class="m-4 flex justify-content-center align-content-start">Welcome to the Intelligent Air Purification System. Register or log in to experience.</label>
-                                    <div class="formStyle flex flex-column gap-2 p-4">
-                                        <label>E-mail</label>
-                                        <InputText id="emailForm" v-model="value"/>
-                                        <small id="email-help">Enter your E-mail aderess.</small>
-                                    </div>
-                                    <div class="flex flex-column gap-2 pr-4 pb-4 pl-4">
-                                        <label>Password</label>
-                                        <InputText id="emailForm" v-model="value"/>
-                                        <small id="Password-help">Enter your Password.</small>
-                                    </div>
-                                    <div class="card flex justify-content-start p-4">
-                                        <Checkbox v-model="Checkbox" :binary="true" />
-                                        <label class="pl-2">I agree to terms of service</label>
-                                    </div>
-                                    <div class="card flex justify-content-center">
-                                        <Button class="buttonLogin w-9 m-4" label="Submit" />
-                                    </div>
-                                </template>
-                            </card>
-                        </div>
-                    </template>
-                    </card>
-                </div>
-            </template>
-        </card>
+<template>
+    <div class="flex justify-content-center mt-6">
+        <img class="imagemLogo" src="/public/LOGORESP.png"/>
+        <div class="cardForm col-4 ">
+            <h1 class="m-4 flex justify-content-center align-content-start">Login</h1>
+            <label class="m-4 flex justify-content-center align-content-start">Welcome to the Intelligent Air Purification System. Register or log in to experience.</label>
+            <div class="formStyle flex flex-column gap-2 p-4">
+                <label>E-mail</label>
+                <InputText id="emailForm" v-model="value"/>
+                <small id="email-help">Enter your E-mail aderess.</small>
+            </div>
+            <div class="flex flex-column gap-2 pr-4 pb-4 pl-4">
+                <label>Password</label>
+                <InputText id="emailForm" v-model="value"/>
+                <small id="Password-help">Enter your Password.</small>
+            </div>
+            <div class="card flex justify-content-start p-4">
+                <Checkbox v-model="Checkbox" :binary="true" />
+                <label class="pl-2">I agree to terms of service</label>
+            </div>
+            <div class="card flex justify-content-center">
+                <Button class="buttonLogin w-9 m-4" label="Submit" />
+            </div>
+        </div>
     </div>
 </template>
 
