8418bf7b6ffc367221f292f3c10e523e2208334a
Maicon Alves <eumaicondouglas.md45@gmail.com>
2024-04-29 19:31:42 -0300

    Style: update footer

--- components/Home/Footer.vue
+++ components/Home/Footer.vue
@@ -3,35 +3,35 @@
     <div class="footer-container">
       <div class="footer-column">
         <h3>Sobre Nós</h3>
-        <p>Somos uma empresa dedicada a fornecer soluções digitais inovadoras.</p>
+        <p>O <strong>RespirAr</strong> é o mais novo sistema de monitoramento da qualidade do ar, projetado para fornecer dados precisos e em tempo real sobre a qualidade do ar em sua comunidade.</p>
       </div>
       <div class="footer-column">
         <h3>Links Rápidos</h3>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Sobre</a></li>
-          <li><a href="#">Serviços</a></li>
-          <li><a href="#">Contato</a></li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">Sobre</a></li>
+          <li><a href="/buy">Serviços</a></li>
+          <li><a href="/contact">Contato</a></li>
         </ul>
       </div>
       <div class="footer-column">
         <h3>Contato</h3>
         <p>Endereço: Rua Exemplo, 123<br>
-           Cidade Exemplo<br>
+           Americana - SP<br>
            Telefone: (00) 1234-5678<br>
-           Email: exemplo@email.com</p>
+           Email: respirar@email.com</p>
       </div>
       <div class="footer-column">
         <h3>Redes Sociais</h3>
         <div class="social-media-icons">
-          <a href="#"><i class="fab fa-facebook-f"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          <a href="#"><i class="pi pi-whatsapp"></i></a>
+          <a href="#"><i class="pi pi-facebook"></i></a>
+          <a href="#"><i class="pi pi-instagram"></i></a>
+          <a href="#"><i class="pi pi-twitter"></i></a>
         </div>
       </div>
     </div>
-    <p>&copy; 2024 Exemplo de Footer. Todos os direitos reservados.</p>
+    <p>&copy; 2024 Respirar. Todos os direitos reservados.</p>
   </footer>
 </template>
 
