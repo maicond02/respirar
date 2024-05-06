680c2e3b00460f828514a22c1dcf2f6ccb04d741
Maicon Alves <AD648ZZ@mmm.com>
2024-05-06 15:28:50 -0300

    Style: Update login page

--- components/User/Login.vue
+++ components/User/Login.vue
@@ -1,27 +1,30 @@
 <template>
-    <div class="flex justify-content-center grid grid-nogutter p-6">
-        <div class="col-12 md:col-6">
-            <img class="imagemLogo border-round-lg border-noround-right" src="/public/LOGORESP.png" height="100%" width="100%"/>
+    <div class="flex justify-content-center grid p-6">
+        <div class="col-12 md:col-6 bg-blue-300 border-round-md">
+            <img class="border-round-lg border-noround-right" src="assets/images/login.png" height="100%" width="100%"/>
         </div>
         <div class="cardForm col-12 md:col-6 border-round-lg border-noround-left">
-            <h1 class="m-4 flex justify-content-center align-content-start">Login</h1>
+            <h1 class="m-4 flex justify-content-center align-content-start">Login - <span class="text-blue-400 ml-2 mr-2">RespirAr Hub</span></h1>
             <label class="m-4 flex justify-content-center align-content-start">Welcome to the Intelligent Air Purification System. Register or log in to experience.</label>
             <div class="formStyle flex flex-column gap-2 p-4">
-                <label>E-mail</label>
+                <label>Nome de Usuário ou E-mail</label>
                 <InputText id="emailForm" v-model="email"/>
-                <small id="email-help">Enter your E-mail aderess.</small>
             </div>
             <div class="flex flex-column gap-2 pr-4 pb-4 pl-4">
-                <label>Password</label>
+                <label>Senha</label>
                 <InputText id="emailForm" v-model="password"/>
-                <small id="Password-help">Enter your Password.</small>
             </div>
-            <div class="card flex justify-content-start p-4">
-                <Checkbox v-model="checkbox" :binary="true" />
-                <label class="pl-2">I agree to terms of service</label>
-            </div>
-            <div class="card flex justify-content-center">
-                <Button class="buttonLogin w-9 m-4" label="Login" />
+            <div class="card flex justify-content-center flex-column align-items-center">
+                <Button class="buttonLogin w-9 m-4" label="Sign in" />
+                <Divider align="center" type="dotted">
+                    <b>OU</b>
+                </Divider>
+                <div class="flex justify-content-center align-items-center mt-4">
+                    <i class="pi pi-google"></i> <span class="ml-2 cursor-pointer">- Entrar com o Google</span>
+                </div>
+                <div class="mt-5">
+                    <span>Novo por aqui?</span> <a class="text-blue-400 underline cursor-pointer">Criar uma conta agora</a>
+                </div>
             </div>
         </div>
     </div>
@@ -31,7 +34,6 @@
     export default{
         data(){
             return{
-                checkbox:false,
                 email:null,
                 password:null
             }
@@ -41,17 +43,9 @@
 
 <style scoped>
 
-.cardLogin{
-    background-color: transparent;
-    border-radius: 0%;
-    height: 100%;
-    width: 100%;
-}
 .cardForm{
     background-color: rgb(32, 32, 32);
 }
-.buttonLogin{
 
-}
 
 </style>
